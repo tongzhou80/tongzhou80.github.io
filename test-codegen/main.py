508dad888a46e17f24af08f2e30d380fca3e2415
Tong Zhou <tong@Tongs-MacBook-Air.local>
2024-10-10 14:22:29 +0800

    now I have 2 editors side by side, to use onclick to implement showing host or device code at a time

--- test-codegen/main.py
+++ test-codegen/main.py
@@ -1,7 +1,7 @@
 from pyweb import pydom
 import ast_comments as ast
 from ast_comments import parse, dump
-from js import getEditor
+from js import getInputCode, setHostCode, setDeviceCode
 
 code = 'a = b   # parallel'
 
@@ -10,11 +10,10 @@ print('hello3')
 #print(dump(parse(code)))
 
 
-page_message = "This example is a code generator."
-pydom["div#page-message"].html = page_message
+# page_message = "This example is a code generator."
+# pydom["div#page-message"].html = page_message
 
 def compile(event):
-    code = pydom["textarea#input-code"][0].value
-    print(code)
-    print(getEditor().getValue())
-    pydom["textarea#output-host-code"][0].value = code
+    code = getInputCode()
+    setHostCode("Host code")
+    setDeviceCode("Device code")
