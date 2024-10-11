e2f779bb826115506d14ca7b2ea16e85c0939864
Tong Zhou <tong@192.168.0.196>
2024-10-11 20:16:10 +0800

    now codegen part works properly; just optimizations do not seem working

--- intrepydd-web/main.py
+++ intrepydd-web/main.py
@@ -1,20 +1,48 @@
 from pyweb import pydom
 import ast_comments as ast
 from ast_comments import parse, dump
-from js import getInputCode, setHostCode, setDeviceCode
+from js import document 
+from js import getInputCode, setInputCode, setHostCode, setDeviceCode
+import intrepydd
 
 print('hello from my script')
 
-import intrepydd
+example_inputs = [
+    '''
+def fib(n: int32) -> int32:
+    if n <= 1: 
+        return n
+    return fib(n - 1) + fib(n - 2)
+    ''',
 
-#print(dump(parse(code)))
+    '''
+def mul_add(a: Array(float64, 1), b: Array(float64, 1)):
+    return 0.5 * a + b 
+    '''
+]
 
+setInputCode(example_inputs[0])
 
 # page_message = "This example is a code generator."
 # pydom["div#page-message"].html = page_message
 
+def show_input(event):
+    id = document.getElementById("input-examples").value
+    setInputCode(example_inputs[int(id)-1])
+
+
 def compile(event):
+    dense_array_opt, sparse_array_opt, licm, slice_opt = [False] * 4
+    if document.getElementById("dense-array-opt").checked:
+        dense_array_opt = True
+
+    # to add code for other opt flags
+
     code = getInputCode()
-    cpp_code = intrepydd.compile_from_src(code)
+    cpp_code = intrepydd.compile_from_src(code, 
+                    dense_array_opt=dense_array_opt,
+                    sparse_array_opt=sparse_array_opt,
+                    licm=licm,
+                    slice_opt=slice_opt)
     setHostCode(cpp_code)
-    setDeviceCode("Device code")
+    
