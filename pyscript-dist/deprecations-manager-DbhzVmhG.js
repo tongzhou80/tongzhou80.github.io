5e0e71c9a82cec14c9e68a36868dc1fbd93944a9
Tong Zhou <zt9465@gmail.com>
2024-11-12 10:25:26 +0800

    added pyscript-dist and a pyodide dir for the top-level index.html

---
+++ pyscript-dist/deprecations-manager-DbhzVmhG.js
@@ -0,0 +1,2 @@
+import{notify as e}from"./error-BWLhIfGl.js";import{e as o}from"./core-B41yp4Hb.js";function r(){const e=document.querySelectorAll("script");for(const o of e)s(o.src)}function s(o){/\/pyscript\.net\/latest/.test(o)&&e("Loading scripts from latest is deprecated and will be removed soon. Please use a specific version instead.")}o.main.onReady.add(r),o.main.onWorker.add(r);
+//# sourceMappingURL=deprecations-manager-DbhzVmhG.js.map
